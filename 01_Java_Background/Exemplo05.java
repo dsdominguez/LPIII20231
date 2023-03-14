1a7d0765af07ece35d18d8bc4da6cb7ce2c38c66
Dany S Dominguez <dsdominguez@gmail.com>
2023-03-14 19:59:40 -0300

    Full examples T1

---
+++ 01_Java_Background/Exemplo05.java
@@ -0,0 +1,24 @@
+
+//Arquivo para exemplificar operações com inteiros e ponto flutuante
+
+public class Exemplo05{
+  // Operações algébricas com variáveis inteiras e de ponto flutuante
+  public static void main(String [] args){
+    double x = 3.1;
+    float y = 2.3f;
+    int a = 3;
+    short b = (short) 2;
+    System.out.println("x = " + x);
+    System.out.println("y = " + y);
+    System.out.println("x + y = " + (x + y));
+    System.out.println("x - y = " + (x - y));
+    System.out.println("x * y = " + (x * y));
+    System.out.println("x / y = " + (x / y));
+    System.out.println("a + y = " + (a + y));
+    System.out.println("x - b = " + (x - b));
+    System.out.println("a * x = " + (a * y));
+    System.out.println("a / b = " + (a / b));
+    System.out.println("b / y = " + (b / y));
+  }
+
+}
