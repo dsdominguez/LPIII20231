bf9e20900597d2bda93b9303ec6195b08ac0fa3c
Dany S Dominguez <dsdominguez@gmail.com>
2023-05-07 20:18:48 -0300

    Upd Estaticos

---
+++ 09_Java_Estaticos/CatExample.java
@@ -0,0 +1,14 @@
+public class CatExample {
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat("Raschid", 1);
+        cat1.info();
+        //cat1.houseName = "OutraCasa";
+
+        Cat cat2 = new Cat("Kayla", 2);
+        cat2.info();
+
+        Cat cat3 = new Cat("Pietro", 0);
+        cat3.info();
+    }
+}
